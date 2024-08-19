d38d5b73f229801b17210a680dc380816989f471
Jeffrich308 <jeffrich308@gmail.com>
2024-08-19 13:29:07 -0500

    update comments

--- Sliders.py
+++ Sliders.py
@@ -13,6 +13,8 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QAction, QSl
 from PyQt5 import uic
 import sys
 
+# Add QSlider to imports
+
 
 class UI(QMainWindow):
     def __init__(self):
